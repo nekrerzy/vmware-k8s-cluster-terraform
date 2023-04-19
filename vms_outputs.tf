75de3c72ee62d696fa5be240dc9b07e27504cc1b
Fernando Navarro <fher.navarro@gmail.com>
2023-04-19 15:32:03 -0600

    added more outputs

--- vms_outputs.tf
+++ vms_outputs.tf
@@ -8,3 +8,13 @@ output "control_planes_names" {
 output "control_planes_ips" {
   value = [for vm in vsphere_virtual_machine.control_planes : vm.default_ip_address]
 }
+
+#output for vm load balancers names
+output "load_balancers_names" {
+  value = [for vm in vsphere_virtual_machine.load_balancers_vm : vm.name]
+}
+
+#output for vm load balancers ips
+output "load_balancers_ips" {
+  value = [for vm in vsphere_virtual_machine.load_balancers_vm : vm.default_ip_address]
+}
